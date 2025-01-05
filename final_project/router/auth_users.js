3fd967b6f39ae6aaf30d196fdb52c53a7901990e
Furqan <furqan.ali8093@gmail.com>
2025-01-05 04:19:24 -0500

    Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,33 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    return users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    const user = user.find(user => user.username === username);
+    return user ? user.password === password: false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password is required"});
+  }
+
+  //Validate username and password
+  if (isValid(username) && authenticatedUser(username, password)) {
+    const token = jwt.sign({ username }, 'secret-key', {expiresIn:'1hr'});
+    
+    return res.status(200).json({message: "Login Successful", token});
+  } else {
+    return res.status(401).json({message: "Invalid username or password"});
+  }
+
 });
 
 // Add a book review
