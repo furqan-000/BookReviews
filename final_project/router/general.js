3d3fea30a1591708a6dc2674208f1dddf798b8d6
Furqan <furqan.ali8093@gmail.com>
2025-01-10 22:15:12 -0500

    Task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,66 +22,63 @@ public_users.post("/register", (req,res) => {
   return res.status(200).json({message: "User successfully registered"});
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop ( Task # 10 - Using Promises )
 public_users.get('/', function (req, res) {
-  const bookList = Object.values(books);
-  res.status(200).json(bookList);
+    new Promise((resolve, reject) => {
+        if (books) {
+          resolve(books);
+        } else {
+          reject("Error: Books not available");
+        }
+      })
+        .then(books => res.status(200).json({ message: "List of books available in the shop:", books }))
+        .catch(err => res.status(500).json({ message: err }));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Get book details based on ISBN ( Task # 11 - ISBN using Async/Await )
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-
-  const book = books[isbn];
-
-  if (book) {
-    res.status(200).json(book);
-  } else {
-    res.status(404).json({message: "Book with ISBN not found"});
+  try {
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+      res.status(200).json({ message: "Book details:", book: books[isbn] });
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
   }
  });
   
-// Get book details based on author
+// Get book details based on author ( Task # 12 - Details based on author using Promises)
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author.toLowerCase();
-
-  const booksByAuthor = [];
-
-  for (const key in books) {
-    if (books[key].author.toLowerCase() === author) {
-        booksByAuthor.push(books[key]);
+  new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const matchingBooks = Object.values(books).filter(book => book.author === author);
+    if (matchingBooks.length > 0) {
+      resolve(matchingBooks);
+    } else {
+      reject("No books found by this author");
     }
-  }
-
-  if (booksByAuthor.length > 0) {
-    res.status(200).json(booksByAuthor);
-
-  } else {
-    res.status(404).json({message: "Books not found by author"});
-  }
-
+  })
+    .then(matchingBooks => res.status(200).json({ message: `Books by author: ${req.params.author}`, books: matchingBooks }))
+    .catch(err => res.status(404).json({ message: err }));
 });
 
-// Get all books based on title
+// Get all books based on title ( Task # 13 - Book details on title using Async/Await)
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  const title = req.params.title.toLowerCase();
-
-  const booksByTitle = [];
-
-  for (const key in books) {
-    if (books[key].title.toLowerCase() === title) {
-        booksByTitle.push(books[key]);
+  try {
+    const title = req.params.title;
+    const matchingBooks = Object.values(books).filter(book => book.title === title);
+    if (matchingBooks.length > 0) {
+      res.status(200).json({ message: `Books with title: ${title}`, books: matchingBooks });
+    } else {
+      res.status(404).json({ message: "No books found with this title" });
     }
-  }
-
-  if (booksByTitle.length > 0) {
-    res.status(200).json(booksByTitle);
-
-  } else {
-    res.status(404).json({message: "No books found with given title"});
+  } catch (err) {
+    res.status(500).json({ message: "An error occurred", error: err.message });
   }
 });
 
