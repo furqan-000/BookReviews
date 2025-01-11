5c5ec1d4b488bfdf10e7c5d25c9fb28ea8d01c5d
Furqan <furqan.ali8093@gmail.com>
2025-01-10 20:44:23 -0500

    Authorization

--- final_project/index.js
+++ final_project/index.js
@@ -10,14 +10,33 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
+app.use("/customer",session({
+    secret:"fingerprint_customer",
+    resave: true, 
+    saveUninitialized: true
+}));
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-    next();
+const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ message: "Authentication token is missing" });
+    }
+
+    const token = authHeader.split(' ')[1]; // Get token from "Bearer <token>"
+    if (!token) {
+        return res.status(401).json({ message: "Authentication token is missing" });
+    }
+
+    jwt.verify(token, "fingerprint_customer", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid token" });
+        }
+        req.user = user;
+        next();
+    });
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
